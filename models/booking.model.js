70b963d679faff16d551a9bbc048e8073bbe6076
mrsweet5 <cherry123@live.com.sg>
2020-08-30 22:04:12 +0800

    models and routes half created

---
+++ models/booking.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+    numberOfAdults: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    
+},{
+    timestamps: true,
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
