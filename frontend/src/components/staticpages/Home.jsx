483e166086dd23367561441beb32b0e150c3f807
mrsweet5 <cherry123@live.com.sg>
2020-09-04 02:32:07 +0800

    homepage

--- frontend/src/components/staticpages/Home.jsx
+++ frontend/src/components/staticpages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div className="container">
-        <img className="navbar-brand js-scroll-trigger" src="https://imgur.com/a/ozdLCT6"/>
+        <img className="navbar-brand js-scroll-trigger" src="assets/img/pusstrap.png"/>
         <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i className="fa fa-bars"></i>
@@ -15,16 +15,16 @@ function Home() {
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav text-uppercase ml-auto">
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#about">About</a>
+              <a className="nav-link js-scroll-trigger" href="#rules">rules</a>
             </li>
             <li className="nav-item">
               <a className="nav-link js-scroll-trigger" href="#team">Team</a>
             </li>
             <li className="nav-item">
               <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="/booking">booking</a>
             </li>
           </ul>
         </div>
@@ -35,45 +35,47 @@ function Home() {
     <header className="masthead">
       <div className="container">
         <div className="intro-text">
-          <div className="intro-heading text-uppercase">how cat i help you?</div>
-          <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+          <div className="intro-heading text-uppercase">how cat<br></br>i help you?<br></br>どんな御用で<br></br>しょうか</div>
+          <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger btn-5" href="/booking">BOOK NOW</a>
         </div>
       </div>
     </header>
   
     
-    <section className="page-section" id="services">
+    <section className="page-section" id="rules">
       <div className="container">
         <div className="row">
           <div className="col-lg-12 text-center">
-            <h2 className="section-heading text-uppercase">Services</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h2 className="section-heading text-uppercase">HOUSE RULES</h2>
+            <h3 className="section-subheading text-muted">Please obey or kick.<br></br>
+              従うかキックしてください。</h3>
           </div>
         </div>
         <div className="row text-center">
           <div className="col-md-4">
             <span className="fa-stack fa-4x">
-              <i className="fa fa-circle fa-stack-2x text-primary"></i>
-              <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+              <i className="fa fa-hand-lizard-o fa-stack-1x fa-inverse"></i>
             </span>
-            <h4 className="service-heading">E-Commerce</h4>
-            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <h4 className="service-heading">Snake Sign</h4>
+            <p className="text-muted">My cats scratch when they see a snake sign. Do not do a snake sign with your hands. I am not liable for any injury.<br></br>私の猫は蛇のサインを見ると引っ掻きます。手で蛇のサインをしないでください。私は怪我の責任を負いません。</p>
           </div>
           <div className="col-md-4">
             <span className="fa-stack fa-4x">
               <i className="fa fa-circle fa-stack-2x text-primary"></i>
-              <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+              <i className="fa fa-address-card-o fa-stack-1x fa-inverse"></i>
             </span>
-            <h4 className="service-heading">Responsive Design</h4>
-            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <h4 className="service-heading">ID Check</h4>
+            <p className="text-muted">Kids are not allowed in my cafe. Kids are human beings below the age of 18. Cats no like kids.<br></br>
+            私のカフェでは子供は許可されていません。子供は18歳未満の人間です。</p>
           </div>
           <div className="col-md-4">
             <span className="fa-stack fa-4x">
               <i className="fa fa-circle fa-stack-2x text-primary"></i>
-              <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+              <i className="fa fa-beer fa-stack-1x fa-inverse"></i>
             </span>
-            <h4 className="service-heading">Web Security</h4>
-            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <h4 className="service-heading">Beer Consumption</h4>
+            <p className="text-muted">You must drink beer and pat my cats. Only beer. No whiskey. Cats love the smell of an alcoholic.<br></br>ビールを飲んで猫をなでなでしょ。ビールだけ。ウイスキーはありません。猫はアルコールの匂いが大好きです。</p>
           </div>
         </div>
       </div>
@@ -82,80 +84,60 @@ function Home() {
     
   
     
-    <section className="page-section" id="about">
+    <section className="page-section" id="team">
       <div className="container">
         <div className="row">
           <div className="col-lg-12 text-center">
-            <h2 className="section-heading text-uppercase">About</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h2 className="section-heading text-uppercase">Team</h2>
+            <h3 className="section-subheading text-muted">Team of money-making fellas.<br></br>金儲けの仲間のチーム。</h3>
           </div>
         </div>
         <div className="row">
           <div className="col-lg-12">
             <ul className="timeline">
               <li>
-                <div className="timeline-image">
+                <div className="timeline-image hi">
                   <img className="rounded-circle img-fluid" src="img/about/1.jpg" alt=""/>
                 </div>
                 <div className="timeline-panel">
                   <div className="timeline-heading">
-                    <h4>2009-2011</h4>
-                    <h4 className="subheading">Our Humble Beginnings</h4>
+                    <h4 className="subheading">Jackson</h4>
                   </div>
                   <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                    <p className="text-muted">Jackson's heart is as black as his fur. Patting him is not recommended. Loves feathers.<br></br>
+                    ジャクソンの心は彼の毛皮と同じくらい黒いです。彼をなでるのはお勧めできません。羽が大好きです。</p>
                   </div>
                 </div>
               </li>
               <li className="timeline-inverted">
-                <div className="timeline-image">
+                <div className="timeline-image hi2">
                   <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt=""/>
                 </div>
                 <div className="timeline-panel">
                   <div className="timeline-heading">
-                    <h4>March 2011</h4>
-                    <h4 className="subheading">An Agency is Born</h4>
+                    <h4 className="subheading">Wilfred</h4>
                   </div>
                   <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                    <p className="text-muted">They say he resembles a fish but I think he resembles a bunny.<br></br>
+                    彼は魚に似ていると彼らは言うが、彼はうさぎに似ていると思う。</p>
                   </div>
                 </div>
               </li>
               <li>
-                <div className="timeline-image">
+                <div className="timeline-image hi3">
                   <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt=""/>
                 </div>
                 <div className="timeline-panel">
                   <div className="timeline-heading">
-                    <h4>December 2012</h4>
-                    <h4 className="subheading">Transition to Full Service</h4>
+                    <h4 className="subheading">Sally</h4>
                   </div>
                   <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                    <p className="text-muted">My little baby girl. Size of a palm. Do not squish.<br></br>
+                    私の小さな女の赤ちゃん。手のひらの大きさ。スキッシュしないでください。</p>
                   </div>
                 </div>
               </li>
-              <li className="timeline-inverted">
-                <div className="timeline-image">
-                  <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt=""/>
-                </div>
-                <div className="timeline-panel">
-                  <div className="timeline-heading">
-                    <h4>July 2014</h4>
-                    <h4 className="subheading">Phase Two Expansion</h4>
-                  </div>
-                  <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                  </div>
-                </div>
-              </li>
-              <li className="timeline-inverted">
-                <div className="timeline-image">
-                  <h4>Be Part
-                    <br/>Of Our
-                    <br/>Story!</h4>
-                </div>
-              </li>
+              
             </ul>
           </div>
         </div>
@@ -163,131 +145,14 @@ function Home() {
     </section>
   
     
-    <section className="bg-light page-section" id="team">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12 text-center">
-            <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-4">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
-              <h4>Kay Garland</h4>
-              <p className="text-muted">Lead Designer</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-facebook-f"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-linkedin-in"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
-              <h4>Larry Parker</h4>
-              <p className="text-muted">Lead Marketer</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-facebook-f"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-linkedin-in"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt=""/>
-              <h4>Diana Pertersen</h4>
-              <p className="text-muted">Lead Developer</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-facebook-f"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa fa-linkedin-in"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-8 mx-auto text-center">
-            <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  
     
-    <section className="py-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3 col-sm-6">
-            <a href="#something">
-              <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt=""/>
-            </a>
-          </div>
-          <div className="col-md-3 col-sm-6">
-            <a href="#something">
-              <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
-            </a>
-          </div>
-          <div className="col-md-3 col-sm-6">
-            <a href="#something">
-              <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
-            </a>
-          </div>
-          <div className="col-md-3 col-sm-6">
-            <a href="#something">
-              <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  
     
     <section className="page-section" id="contact">
       <div className="container">
         <div className="row">
           <div className="col-lg-12 text-center">
             <h2 className="section-heading text-uppercase">Contact Us</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 className="section-subheading text-muted">Don't contact me.</h3>
           </div>
         </div>
         <div className="row">
